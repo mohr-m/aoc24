50af2686e74c47e5c1c75024e4a43ff8405bf3f1
Moritz Mohr <m.mohr.1@campus.tu-berlin.de>
2024-12-14 19:27:23 +0100

    add day_9 task_2

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -18,7 +18,6 @@ def build_memory(f_map):
 
 def defrag_memory(memory):
     j = 0
-    # breakpoint()
     for i in range(len(memory)-1, -1, -1):
         if i < j:
             break
@@ -31,21 +30,57 @@ def defrag_memory(memory):
             memory[i] = None
     return memory
 
-def checksum_of(memory):
+def move_blocks(f_map):
+    for i, m in enumerate(f_map):
+        f_map[i] = (int(i/2) if i % 2 == 0 else None, int(m))
+    i = len(f_map) - 1
+    while i > 0:
+        if f_map[i][0] == None:
+            i -= 1
+            continue
+        for j in range(0, i):
+            v = f_map[j]
+            if v[0] == None and v[1] >= f_map[i][1]:
+                rest = v[1] - f_map[i][1]
+                f_map[j] = f_map[i]
+                if rest: 
+                    f_map.insert(j+1, (None, rest))
+                    f_map[i + 1] = (None, f_map[j][1])
+                    i += 1
+                else:
+                    f_map[i] = (None, f_map[j][1])
+                break
+        i -= 1
+    return f_map
+
+def f_map2str(f):
+    res = []
+    for m in f:
+        res.extend([m[0]] * m[1])
+    return res
+
+def mem_checksum(memory):
     res = 0
     for i, mi in enumerate(memory):
         if mi != None:
             res += i * mi
     return res
+
+def f_map_checksum(mem_str):
+    res = 0
+    for i, v in enumerate(mem_str):
+        if v != None:
+            res += i * v
+    return res
     
 def task_1(input_file):
     f_map = read_input(input_file)
     memory = build_memory(f_map)
-    print(checksum_of(defrag_memory(memory)))
-    
+    print(mem_checksum(defrag_memory(memory)))
 
 def task_2(input_file):
-    pass
+    f_map = list(read_input(input_file))
+    print(f_map_checksum(f_map2str(move_blocks(f_map))))
 
 if __name__ == "__main__":
     input_file = 'input.txt'
